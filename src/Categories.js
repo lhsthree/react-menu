c791f16a53c4fd460cee01399bb29d7395c7cb21
lhsthree <lhsthree@gmail.com>
2021-02-02 16:38:42 -0600

    feat: add new set for category

--- src/Categories.js
+++ src/Categories.js
@@ -1,24 +1,15 @@
 import React from 'react';
 
-const Categories = ({ filterItems }) => {
-  return <div className="btn-container">
-  	<button className="filter-btn" onClick={() => 
-  		filterItems('all')}>
-  		all
-  	</button>
-  	<button className="filter-btn" onClick={() => 
-  		filterItems('breakfast')}>
-  		breakfast
-  	</button>
-  	<button className="filter-btn" onClick={() => 
-  		filterItems('lunch')}>
-  		lunch
-  	</button>
-  	<button className="filter-btn" onClick={() => 
-  		filterItems('shakes')}>
-  		shakes
-  	</button>
-  </div>;
+const Categories = ({ categories, filterItems }) => {
+  return (<div className="btn-container">
+    	{categories.map((category, index) => {
+    		return <button key={index} className="filter-btn" onClick={() => 
+    		filterItems(category)}>
+    		{category}
+    	</button>
+    	})
+    	}
+    </div>);
 };
 
 export default Categories;
